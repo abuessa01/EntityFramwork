33df45afc951feb0dd3fe520118a53d8009bcae3
cherishla <cherishla.fang@gmail.com>
2016-06-08 11:03:13 +0800

    02建立 EDMX 並輸出 dbo.Course 表格中所有資料

---
+++ EFConsole/EFConsole/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new ContosoUniversityEntities())
+            {
+                Console.WriteLine("\t");
+                Console.WriteLine("demo1-------------------\t");
+                foreach (var item in db.Course)
+                {
+                    Console.WriteLine(item.Title + "\t" + item.DepartmentID);
+
+                }
+                Console.WriteLine("\t");
+                Console.WriteLine("demo2-------------------\t");
+                foreach (var item in db.Department)
+                {
+                    Console.WriteLine(item.Name);
+                    foreach (var course in item.Course)
+                    {
+                        Console.WriteLine("\t" + course.Title);
+                    }
+                }
+            }
+        }
+    }
+}
