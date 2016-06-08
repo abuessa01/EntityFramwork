fed5a58879782709103bea7fcd149a48d545de3b
cherishla <cherishla.fang@gmail.com>
2016-06-08 15:30:45 +0800

    x

--- EFConsole/EFConsole/Program.cs
+++ EFConsole/EFConsole/Program.cs
@@ -10,77 +10,81 @@ namespace EFConsole
     {
         static void Main(string[] args)
         {
-            var c = new Course();
-            using (var db = new ContosoUniversityEntities())
-            {
-                #region mark
-                //Console.WriteLine("\t");
-                //Console.WriteLine("demo1-------------------\t");
-                //foreach (var item in db.Course)
-                //{
-                //    Console.WriteLine(item.Title + "\t" + item.DepartmentID);
+            #region mark
+            //using (var db = new ContosoUniversityEntities())
+            //{
 
-                //}
-                //Console.WriteLine("\t");
-                //Console.WriteLine("demo2-------------------\t");
-                //foreach (var item in db.Department)
-                //{
-                //    Console.WriteLine(item.Name);
-                //    foreach (var course in item.Course)
-                //    {
-                //        Console.WriteLine("\t" + course.Title);
-                //    }
-                //}
+            #region demo1 利用EF撈資料
+            //Console.WriteLine("\t");
+            //Console.WriteLine("demo1-------------------\t");
+            //foreach (var item in db.Course)
+            //{
+            //    Console.WriteLine(item.Title + "\t" + item.DepartmentID);
 
-                //Console.WriteLine("\t");
-                //Console.WriteLine("demo3-------------------\t");
-                ////新增
-                //var newCouse = new Course()
-                //{
-                //    CourseID = 0,
-                //    Title = "EF",
-                //    Credits = 1,
-                //    DepartmentID = 2,
-                //    CreateOn = DateTime.Now.AddDays(1)
+            //}
+            #endregion
 
-                //};
-                //db.Course.Add(newCouse);
-                ////更新
-                //var c = db.Course.Find(8);
-                //c.ModifyOn = DateTime.Now;
-                //db.SaveChanges();
+            #region demo2 利用EF撈資料(1對多)
+            //Console.WriteLine("\t");
+            //Console.WriteLine("demo2-------------------\t");
+            //foreach (var item in db.Department)
+            //{
+            //    Console.WriteLine(item.Name);
+            //    foreach (var course in item.Course)
+            //    {
+            //        Console.WriteLine("\t" + course.Title);
+            //    }
+            //}
+            #endregion
 
-                //Console.WriteLine("\t");
-                //Console.WriteLine("demo4: trace sql-------------------\t");
-                //db.Database.Log = (msg) =>
-                //{
-                //    Console.WriteLine(msg);
-                //};
+            #region dmmo3 CU data
+            //Console.WriteLine("\t");
+            //Console.WriteLine("demo3-------------------\t");
+            ////新增
+            //var newCouse = new Course()
+            //{
+            //    CourseID = 0,
+            //    Title = "EF",
+            //    Credits = 1,
+            //    DepartmentID = 2,
+            //    CreateOn = DateTime.Now.AddDays(1)
 
-                //Console.WriteLine("\t");
-                //Console.WriteLine("demo4:狀態-------------------\t");
-                //var c = db.Course.Find(8);
-                //var ce = db.Entry(c);
-                //Console.WriteLine(ce.State);
-                ////db.Course.Remove(c);
-                ////直接變動狀態也可進行資料異動
-                //c.Title = "EF2";
-                //db.SaveChanges();
-                ////複製
-                ////db.Entry(c).State = System.Data.Entity.EntityState.Added;
-                #endregion
+            //};
+            //db.Course.Add(newCouse);
+            ////更新
+            //var c = db.Course.Find(8);
+            //c.ModifyOn = DateTime.Now;
+            //db.SaveChanges();
+            #endregion
 
-                c = db.Course.Find(8);
-                c.Credits += 1;
-            }
+            #region demo4 trace sql
+            //Console.WriteLine("\t");
+            //Console.WriteLine("demo4: trace sql-------------------\t");
+            //db.Database.Log = (msg) =>
+            //{
+            //    Console.WriteLine(msg);
+            //};
 
-            using (var db1 = new ContosoUniversityEntities())
-            {
-                db1.Entry(c).State = System.Data.Entity.EntityState.Modified;
-                db1.SaveChanges();
-            }
+            //Console.WriteLine("\t");
+            #endregion
 
-            #region demo 離線
+            #region demo5 entry.state
+            //Console.WriteLine("demo4:狀態-------------------\t");
+            //var c = db.Course.Find(8);
+            //var ce = db.Entry(c);
+            //Console.WriteLine(ce.State);
+            ////db.Course.Remove(c);
+            ////直接變動狀態也可進行資料異動
+            //c.Title = "EF2";
+            //db.SaveChanges();
+            ////複製
+            ////db.Entry(c).State = System.Data.Entity.EntityState.Added;
+            #endregion
+
+            //}
+
+
+            #region demo6 離線
             //    Console.WriteLine("\t");
             //Console.WriteLine("demo5:離線-------------------\t");
             //var d = new Course()
@@ -99,6 +103,26 @@ namespace EFConsole
             //    db1.SaveChanges();
             //}
             #endregion
+
+            #region demo7 離線(2個生命週期
+            //var c = new Course();
+            //using (var db = new ContosoUniversityEntities())
+            //{
+            //    c = db.Course.Find(8);
+            //    c.Credits += 1;
+            //}
+            //    using (var db1 = new ContosoUniversityEntities())
+            //{
+            //    db1.Entry(c).State = System.Data.Entity.EntityState.Modified;
+            //    db1.SaveChanges();
+            //}
+
+            #endregion
+
+            #endregion
+            
+
         }
     }
+
 }
