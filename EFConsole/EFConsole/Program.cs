0ce1ca8f57af2f1802f303f33290174567d873a5
cherishla <cherishla.fang@gmail.com>
2016-06-08 11:23:55 +0800

    建立 CreatedOn 與 ModifiedOn 欄位 (datetime)，並適當設定 EDMX 的 StoredGeneratedPattern="Computed" 屬性，最後新增與修改一筆資料

--- EFConsole/EFConsole/Program.cs
+++ EFConsole/EFConsole/Program.cs
@@ -12,23 +12,43 @@ namespace EFConsole
         {
             using (var db = new ContosoUniversityEntities())
             {
-                Console.WriteLine("\t");
-                Console.WriteLine("demo1-------------------\t");
-                foreach (var item in db.Course)
-                {
-                    Console.WriteLine(item.Title + "\t" + item.DepartmentID);
+                //Console.WriteLine("\t");
+                //Console.WriteLine("demo1-------------------\t");
+                //foreach (var item in db.Course)
+                //{
+                //    Console.WriteLine(item.Title + "\t" + item.DepartmentID);
 
-                }
+                //}
+                //Console.WriteLine("\t");
+                //Console.WriteLine("demo2-------------------\t");
+                //foreach (var item in db.Department)
+                //{
+                //    Console.WriteLine(item.Name);
+                //    foreach (var course in item.Course)
+                //    {
+                //        Console.WriteLine("\t" + course.Title);
+                //    }
+                //}
+
+
                 Console.WriteLine("\t");
-                Console.WriteLine("demo2-------------------\t");
-                foreach (var item in db.Department)
+                Console.WriteLine("demo3-------------------\t");
+
+                var newCouse = new Course()
                 {
-                    Console.WriteLine(item.Name);
-                    foreach (var course in item.Course)
-                    {
-                        Console.WriteLine("\t" + course.Title);
-                    }
-                }
+                    CourseID = 8,
+                    Title = "EF",
+                    Credits = 1,
+                    DepartmentID = 2,
+                    CreateOn = DateTime.Now
+
+                };
+                db.Course.Add(newCouse);
+                var c = db.Course.Find(8);
+                c.ModifyOn = DateTime.Now;
+                db.SaveChanges();
+
+                
             }
         }
     }
